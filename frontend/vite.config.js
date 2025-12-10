b8b9f14fdb68cd882654da464e9386e906c0dfd9
Bernardo Fusco <bernardol.fusco@gmail.com>
2025-12-10 16:05:07 -0300

    ajuste no vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'SmartAdega';
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
-      }
+    plugins: [react()],
+
+    // Necessário para GitHub Pages
+    base: `/${repoName}/`,
+
+    server: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3001',
+                changeOrigin: true,
+            }
+        }
     }
-  }
 })
